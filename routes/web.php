74592b425637f0ed7bc806a6abae31b3de74c179
petrero <petrero@gmail.com>
2022-11-29 10:52:53 +0200

    6.1 Create a fake post request to log out. If we click on log out link we will fail because we doesn't register a get request to log out

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,15 @@ Route::get('/', function () {
     return Inertia::render('Home');
 });
 
-Route::get('users', function(){
+Route::get('/users', function(){
     sleep(2);
     return Inertia::render('Users');
 });
 
-Route::get('settings', function(){
+Route::get('/settings', function(){
     return Inertia::render('Settings');
+});
+
+Route::post('/logout', function(){
+    dd('logging user out');
 });
