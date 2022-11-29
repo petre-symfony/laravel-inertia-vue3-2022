c67ab7d7b342bca01f5714e8f12b8b523fe7889f
petrero <petrero@gmail.com>
2022-11-29 08:39:43 +0200

    4.2 Make some traditional links to navigate between pages. But the use of this defeats the entire of single page application. That's a traditional application that loads a vue component

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,12 @@
 <template>
-Home
+    <h1>Home</h1>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/users">Users</a></li>
+            <li><a href="/settings">Settings</a></li>
+        </ul>
+    </nav>
 </template>
 
 <script>
