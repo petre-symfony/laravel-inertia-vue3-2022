9ecd561f3b0b15cdaae8c8f3396c87b8f1f2b1fe
petrero <petrero@gmail.com>
2022-11-29 11:11:00 +0200

    6.3 On mac I can command+click on link. I don't want to render an anchor tag in this case because we get the same error as before. We want something like a button

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -4,7 +4,7 @@
             <li><Link href="/">Home</Link></li>
             <li><Link href="/users">Users</Link></li>
             <li><Link href="/settings">Settings</Link></li>
-            <li><Link href="/logout" method="POST">Log Out</Link></li>
+            <li><Link href="/logout" method="POST" as="button">Log Out</Link></li>
         </ul>
     </nav>
 </template>
