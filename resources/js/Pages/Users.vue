c4ccf996af2b200b8491e9991130e1ab693bbc78
petrero <petrero@gmail.com>
2022-11-29 12:22:49 +0200

    7.1 When click on Refresh link we are getting on top of the users page after that.

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -2,13 +2,24 @@
     <h1>Users</h1>
 
     <Nav />
+
+    <div style="margin-top: 400px">
+    The current time is {{ time }}
+    </div>
+
+    <Link href="/users" class="text-blue-500">Refresh</Link>
 </template>
 
 <script>
 import Nav from '../Shared/Nav'
+import { Link } from '@inertiajs/inertia-vue3'
+
 export default {
     name: "Users",
-    components: { Nav }
+    components: { Nav, Link },
+    props: {
+        time: String
+    }
 }
 </script>
 
