6f989f1f76788eac7a7cc0808dc9d428bee346b1
petrero <petrero@gmail.com>
2022-12-01 10:47:05 +0200

    12.1 In its current state  Layout shared component has a problem. The Layout is actually a child of each other component, not the other way around, so evert time I browse from page to page, the Layout is being destroyed and rebuilt. You can see this because when I type something in the input and I browse to another page immediately after the value of the input is changed back to "My App" That's a proof that the Layout is destroyed and rebuilt when navigate around

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -2,7 +2,9 @@
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
             <div class="flex items-center">
-                <h1 class="font-bold text-lg">My App</h1>
+                <h1 class="font-bold text-lg">
+                    <input type="text" value="My App">
+                </h1>
 
                 <p class="text-sm ml-4">Welcome back, {{ username }}</p>
             </div>
