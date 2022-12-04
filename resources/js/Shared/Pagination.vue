56d861b7f3024f8c1ba6acf7d380d9145e9a67d5
petrero <petrero@gmail.com>
2022-12-04 14:43:00 +0200

    17.10 Highlight the page we're currently on

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -5,7 +5,7 @@
         :href="link.url"
         v-html="link.label"
         class="px-1"
-        :class="link.url ? '' : 'text-gray-500'"
+        :class="{ 'text-gray-500': !link.url, 'font-bold': link.active }"
     />
 </template>
 
