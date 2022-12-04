e239e1bbf59e37e8007ed4c08c1be5c5db02081d
petrero <petrero@gmail.com>
2022-12-04 14:35:15 +0200

    17.8 Share pagination across any page

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -34,19 +34,13 @@
     <!-- Paginator -->
 
     <div class="mt-6">
-        <Component
-            v-for="link in users.links"
-            :is="link.url ? 'Link' : 'span'"
-            :href="link.url"
-            v-html="link.label"
-            class="px-1"
-            :class="link.url ? '' : 'text-gray-500'"
-        />
+        <Pagination :links="users.links"/>
     </div>
 
 </template>
 
 <script setup>
+    import Pagination from '../Shared/Pagination'
 
     defineProps({
         users: Object
---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,20 @@
+<template>
+    <Component
+        v-for="link in links"
+        :is="link.url ? 'Link' : 'span'"
+        :href="link.url"
+        v-html="link.label"
+        class="px-1"
+        :class="link.url ? '' : 'text-gray-500'"
+    />
+</template>
+
+<script setup>
+    defineProps({
+       links: Array
+    })
+</script>
+
+<style scoped>
+
+</style>
