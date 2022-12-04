01a1666aaca8be0bd412e73e3bc31ad0b94f456c
petrero <petrero@gmail.com>
2022-12-04 09:56:54 +0200

    15.2 Create a title for Home page using Head component provided by inertia

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,8 +1,13 @@
 <template>
+    <Head>
+        <title>My App - Home</title>
+    </Head>
     <h1 class="text-3xl">Home</h1>
 </template>
 
-
+<script setup>
+    import { Head } from '@inertiajs/inertia-vue3'
+</script>
 
 <style scoped>
 
