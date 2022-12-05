dc5d691b543310696f7224d23e9f92dd62a6be24
petrero <petrero@gmail.com>
2022-12-05 17:41:12 +0200

    19.2 Add a form in our create users page

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -1,5 +1,14 @@
 <template>
-Hello world
+    <Head title="Create User"/>
+
+    <h1 class="text-3xl">Create New User</h1>
+
+    <form action="" method="POST">
+        <div class="mb-6">
+            <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
+            <input type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
+        </div>
+    </form>
 </template>
 
 <script>
