1e3b03a4506b376c190165771eb0482791863d50
petrero <petrero@gmail.com>
2022-12-05 18:00:19 +0200

    19.4 Track what the user types in the form fields

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -6,17 +6,17 @@
     <form action="" method="POST" class="max-w-md mx-auto mt-8">
         <div class="mb-6">
             <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
-            <input type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
+            <input v-model="form.name" type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
         </div>
 
         <div class="mb-6">
             <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">Email</label>
-            <input type="text" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
+            <input v-model="form.email" type="text" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
         </div>
 
         <div class="mb-6">
             <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
-            <input type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
+            <input v-model="form.password" type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
         </div>
 
         <div class="mb-6">
@@ -25,10 +25,14 @@
     </form>
 </template>
 
-<script>
-export default {
-    name: "Create"
-}
+<script setup>
+    import { reactive } from 'vue'
+
+    let form = reactive({
+        name: '',
+        email: '',
+        password: ''
+    })
 </script>
 
 <style scoped>
