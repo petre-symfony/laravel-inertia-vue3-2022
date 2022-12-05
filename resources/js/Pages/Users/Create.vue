9383bc0299da077cceb2bf75875528c4f4b21279
petrero <petrero@gmail.com>
2022-12-05 19:07:50 +0200

    20.5 Put required attribute back in the form

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -6,19 +6,19 @@
     <form @submit.prevent="submit" method="POST" class="max-w-md mx-auto mt-8">
         <div class="mb-6">
             <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
-            <input v-model="form.name" type="text" class="border border-gray-400 p-2 w-full" name="name" id="name">
+            <input v-model="form.name" type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
             <div v-if="errors.name" v-text="errors.name" class="text-red-500 text-xs mt-1" />
         </div>
 
         <div class="mb-6">
             <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">Email</label>
-            <input v-model="form.email" type="text" class="border border-gray-400 p-2 w-full" name="email" id="email">
+            <input v-model="form.email" type="text" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
             <div v-if="errors.email" v-text="errors.email" class="text-red-500 text-xs mt-1" />
         </div>
 
         <div class="mb-6">
             <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
-            <input v-model="form.password" type="password" class="border border-gray-400 p-2 w-full" name="password" id="password">
+            <input v-model="form.password" type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
             <div v-if="errors.password" v-text="errors.password" class="text-red-500 text-xs mt-1" />
         </div>
 
