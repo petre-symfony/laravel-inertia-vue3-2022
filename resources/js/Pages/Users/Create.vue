3a6b20c0a6297a1d039ed46c7648e084604863de
petrero <petrero@gmail.com>
2022-12-05 20:25:58 +0200

    21.3 useForm inertia vue3 helper track the user input, track the validation errors and the processing status automatically for us

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -7,53 +7,44 @@
         <div class="mb-6">
             <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
             <input v-model="form.name" type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
-            <div v-if="errors.name" v-text="errors.name" class="text-red-500 text-xs mt-1" />
+            <div v-if="form.errors.name" v-text="form.errors.name" class="text-red-500 text-xs mt-1" />
         </div>
 
         <div class="mb-6">
             <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">Email</label>
             <input v-model="form.email" type="text" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
-            <div v-if="errors.email" v-text="errors.email" class="text-red-500 text-xs mt-1" />
+            <div v-if="form.errors.email" v-text="form.errors.email" class="text-red-500 text-xs mt-1" />
         </div>
 
         <div class="mb-6">
             <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
             <input v-model="form.password" type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
-            <div v-if="errors.password" v-text="errors.password" class="text-red-500 text-xs mt-1" />
+            <div v-if="form.errors.password" v-text="form.errors.password" class="text-red-500 text-xs mt-1" />
         </div>
 
         <div class="mb-6">
             <button
                 type="submit"
                 class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
-                :disabled="processing"
+                :disabled="form.processing"
             >Submit</button>
         </div>
     </form>
 </template>
 
 <script setup>
-    import { reactive, ref } from 'vue'
-    import { Inertia } from "@inertiajs/inertia"
+    import { useForm } from '@inertiajs/inertia-vue3'
 
-    let form = reactive({
+    let form = useForm({
         name: '',
         email: '',
         password: ''
     })
 
-    let processing = ref(false)
-
     let submit = () => {
-        Inertia.post('/users', form, {
-            onStart: () => { processing.value = true },
-            onFinish: () => { processing.value = false }
-        })
+        form.post('/users')
     }
 
-    defineProps({
-        errors: Object
-    })
 </script>
 
 <style scoped>
