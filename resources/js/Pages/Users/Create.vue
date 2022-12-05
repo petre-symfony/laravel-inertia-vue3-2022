9d020255a01c7e46273bacdb73d4bea3fb3c46b8
petrero <petrero@gmail.com>
2022-12-05 17:52:46 +0200

    19.3 Further style the form and add new fields

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -3,10 +3,24 @@
 
     <h1 class="text-3xl">Create New User</h1>
 
-    <form action="" method="POST">
+    <form action="" method="POST" class="max-w-md mx-auto mt-8">
         <div class="mb-6">
             <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
             <input type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
+        </div>
+
+        <div class="mb-6">
+            <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">Email</label>
+            <input type="text" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
+        </div>
+
+        <div class="mb-6">
+            <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
+            <input type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
+        </div>
+
+        <div class="mb-6">
+            <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Submit</button>
         </div>
     </form>
 </template>
