af9c6bf43f986da99b9e1df8bb7e15c713e95192
petrero <petrero@gmail.com>
2022-12-05 18:59:07 +0200

    20.3 Display errors information when validation failes

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -7,16 +7,19 @@
         <div class="mb-6">
             <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
             <input v-model="form.name" type="text" class="border border-gray-400 p-2 w-full" name="name" id="name">
+            <div v-if="$page.props.errors.name" v-text="$page.props.errors.name" class="text-red-500 text-xs mt-1" />
         </div>
 
         <div class="mb-6">
             <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">Email</label>
             <input v-model="form.email" type="text" class="border border-gray-400 p-2 w-full" name="email" id="email">
+            <div v-if="$page.props.errors.email" v-text="$page.props.errors.email" class="text-red-500 text-xs mt-1" />
         </div>
 
         <div class="mb-6">
             <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
             <input v-model="form.password" type="password" class="border border-gray-400 p-2 w-full" name="password" id="password">
+            <div v-if="$page.props.errors.password" v-text="$page.props.errors.password" class="text-red-500 text-xs mt-1" />
         </div>
 
         <div class="mb-6">
