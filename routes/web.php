45912229ff33ab28da9db6d60a8283a756371152
petrero <petrero@gmail.com>
2022-12-06 23:14:12 +0200

    24.4 Adopt can syntax present in Laravel 9 in the create users route

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::middleware('auth')->group(function(){
 
     Route::get('/users/create', function(){
         return Inertia::render('Users/Create');
-    })->middleware('can:create, App\Models\User');
+    })->can('create', 'App\Models\User');
 
     Route::post('/users', function(){
         //validate the request
